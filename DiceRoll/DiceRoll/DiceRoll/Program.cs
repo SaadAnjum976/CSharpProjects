7c1acf56cb11eaad954938de0c0ca896a02de206
Saad.Anjum976 <cssaad.anjum@gmail.com>
2024-02-26 20:21:12 -0600

    Initial Commit

---
+++ DiceRoll/DiceRoll/DiceRoll/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+
+        while (true)
+        {
+            Console.WriteLine("Press enter to roll the dice, or type 'quit' to exit.");
+            string input = Console.ReadLine();
+
+            if (input == "quit")
+            {
+                break;
+            }
+
+            int roll1 = rand.Next(1, 7);
+            int roll2 = rand.Next(1, 7);
+
+            Console.WriteLine("You rolled a {0} and a {1}.", roll1, roll2);
+
+            if (roll1 + roll2 == 7)
+            {
+                Console.WriteLine("You win!");
+            }
+            else
+            {
+                Console.WriteLine("You lose.");
+            }
+        }
+    }
+}
